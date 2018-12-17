156e04637c3ab510315481651b95bccfd9bad029
Jason <j.mortfield44@gmail.com>
2018-12-17 17:52:39 -0500

    Added function to add color to skills upon scroll, styled form and footer, begun media queries

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,106 @@ $(function(){
 
     $(".header__hamburgerMenu").on("click", function () {
         $(this).toggleClass("hidden");
-        $(".header__nav-list").toggleClass("hidden");
+        $(".header__nav--list").toggleClass("hidden");
     });
 
-    $(".header__nav-exitItem").on("click", function () {
+    $(".header__nav--exitItem").on("click", function () {
         $(".header__hamburgerMenu").toggleClass("hidden");
-        $(".header__nav-list").toggleClass("hidden");
+        $(".header__nav--list").toggleClass("hidden");
     })
+
+
+
+
+
+    $(".skills__itemRowOne").waypoint(
+      function() {
+        $(".skills__itemRowOne").addClass("addColor");
+      },
+      {
+        offset: "75%"
+      }
+    );
+
+    // $(".skills__itemRowOne").waypoint(
+    //   function() {
+    //     $(".skills__itemRowOne").removeClass("addColor");
+    //   },
+    //   {
+    //     offset: "20%"
+    //   }
+    // );
+
+    $(".skills__itemRowTwo").waypoint(
+      function() {
+        $(".skills__itemRowTwo").addClass("addColor");
+      },
+      {
+        offset: "75%"
+      }
+    );
+    
+    // $(".skills__itemRowTwo").waypoint(
+    //   function() {
+    //     $(".skills__itemRowTwo").removeClass("addColor");
+    //   },
+    //   {
+    //     offset: "20%"
+    //   }
+    // );
+
+    $(".skills__itemRowThree").waypoint(
+      function() {
+        $(".skills__itemRowThree").addClass("addColor");
+      },
+      {
+        offset: "75%"
+      }
+    );
+
+    // $(".skills__itemRowThree").waypoint(
+    //   function() {
+    //     $(".skills__itemRowThree").removeClass("addColor");
+    //   },
+    //   {
+    //     offset: "20%"
+    //   }
+    // );
+
+    $(".skills__itemRowFour").waypoint(
+      function() {
+        $(".skills__itemRowFour").addClass("addColor");
+      },
+      {
+        offset: "75%"
+      }
+    );
+
+    // $(".skills__itemRowFour").waypoint(
+    //   function() {
+    //     $(".skills__itemRowFour").removeClass("addColor");
+    //   },
+    //   {
+    //     offset: "20%"
+    //   }
+    // );
+
+    $(".skills__itemRowFive").waypoint(
+      function() {
+        $(".skills__itemRowFive").addClass("addColor");
+      },
+      {
+        offset: "75%"
+      }
+    );
+
+    // $(".skills__itemRowFive").waypoint(
+    //   function() {
+    //     $(".skills__itemRowFive").removeClass("addColor");
+    //   },
+    //   {
+    //     offset: "20%"
+    //   }
+    // );
 
 })
